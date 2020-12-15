afc2884a309106a8e3e76e85b9ea38706048e47e
Junior <mauricio.de.o.junior@accenture.com>
2020-12-15 14:44:02 -0300

    Adjustment

--- OrangeHRM/Script/Unit1.js
+++ OrangeHRM/Script/Unit1.js
@@ -1,4 +1,5 @@
-﻿function LoginPage()
+﻿//@TestScript
+function LoginPage()
 {
   //Launches the specified browser and opens the specified URL in it.
   Browsers.Item(btChrome).Run("https://opensource-demo.orangehrmlive.com/index.php/auth/login");
@@ -12,4 +13,53 @@
   aqObject.CheckProperty(Aliases.browser.pageOrangehrm.panelBranding.linkWelcome, "contentText", cmpEqual, "Welcome Lee");
   //Closes the 'Item' window.
   Aliases.browser.Item.Close();
+}
+
+//@TestParameters
+function Main()
+{
+  processCommandLineArguments();
+}
+
+//@TestParameters
+function processCommandLineArguments()
+{
+  for (var i = 0; i < BuiltIn.ParamCount(); i++)
+  {
+    processCommandLineArgument(BuiltIn.ParamStr(i))
+  }
+}
+
+//@TestParameters
+function processCommandLineArgument(arg)
+{
+  var items = arg.split("=");
+
+  if (items.length != 2)
+  {
+    return;
+  }
+
+  var item = aqString.ToLower(aqString.Replace(aqString.Trim(items[0]), "/", ""));
+  if (item == "config")
+  {
+    // Run the Test routine with the parameter value
+    Test(items[1]);
+  }
+}
+
+//@TestParameters
+function Test(config)
+{
+  switch (aqString.ToLower(aqString.Trim(config)))
+  {
+    case "SVT":
+      Log.Message("Running tests for the SVT configuration");
+      // Simulate a user action over the debug version of the application
+      break;
+    case "PTST":
+      Log.Message("Running tests for the PTST configuration");
+      // Simulate a user action over the debug version of the application
+      break;
+  }
 }
